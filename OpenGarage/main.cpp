9012f31f9eaa05789b759b9667a720b0434f3572
brendelj <larry.brendel@gmail.com>
2026-08-02 20:47:51 -0500

    Assemble modernized OpenGarage main firmware

---
+++ OpenGarage/main.cpp
@@ -0,0 +1,8 @@
+// OpenGarage main firmware implementation.
+// Split into include fragments in this fork only to keep the modernization
+// changes easy to review while preserving one translation unit.
+#include "main_part1.inc"
+#include "main_part2a.inc"
+#include "main_part2b.inc"
+#include "main_part3a.inc"
+#include "main_part3b.inc"
