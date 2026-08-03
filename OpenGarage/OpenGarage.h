5e76fcc0043b1659f475d55b93a36b530c81c4a2
brendelj <larry.brendel@gmail.com>
2026-08-02 20:39:59 -0500

    Import upstream OpenGarage header

---
+++ OpenGarage/OpenGarage.h
@@ -0,0 +1,107 @@
+/* OpenGarage Firmware
+ *
+ * OpenGarage library header file
+ * Mar 2016 @ OpenGarage.io
+ *
+ * This file is part of the OpenGarage library
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _OPENGARAGE_H
+#define _OPENGARAGE_H
+
+#include <Arduino.h>
+#include <FS.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <DHTesp.h>
+#include <DallasTemperature.h>
+#include <EMailSender.h>
+#include <garagelib.cpp>
+#include "defines.h"
+
+struct OptionStruct {
+	String name;
+	uint ival;
+	uint max;
+	String sval;
+};
+
+struct LogStruct {
+	ulong tstamp;
+	uint status;
+	uint dist;
+	byte sn2;
+};
+
+class OpenGarage {
+public:
+	static OptionStruct options[];
+	static byte state;
+	static byte alarm;
+	static byte alarm_action;
+	static byte led_reverse;
+	static byte has_swrx;
+	static void begin();
+	static void options_setup();
+	static void options_load();
+	static void options_save();
+	static void options_reset();
+	static void restart() { ESP.restart();}
+	static uint read_distance();
+	static void init_sensors();
+	static void read_TH_sensor(float& C, float &H);
+	static byte get_mode()   { return options[OPTION_MOD].ival; }
+	static byte get_button() { return digitalRead(PIN_BUTTON); }
+	static byte get_switch() { pinMode(PIN_SWITCH, INPUT_PULLUP); return digitalRead(PIN_SWITCH); }
+	static byte get_led()    { return led_reverse?(!digitalRead(PIN_LED)):digitalRead(PIN_LED); }
+	static void set_led(byte status)   { digitalWrite(PIN_LED, led_reverse?(!status):status); }
+	static void set_relay(byte status) { digitalWrite(PIN_RELAY, status); }
+	static void click_relay() {
+		set_relay(HIGH);
+		delay(options[OPTION_CDT].ival);
+		set_relay(LOW);
+	}
+	static int find_option(String name);
+	static void log_reset();
+	static void write_log(const LogStruct& data);
+	static bool read_log_start();
+	static bool read_log_next(LogStruct& data);
+	static bool read_log_end();
+	static void play_note(uint freq);
+	static void set_alarm(byte ov=0, byte action=ACTION_TOGGLE) {
+		if(ov) alarm = ov*10+1;
+		else alarm = options[OPTION_ALM].ival * 10 + 1;
+		alarm_action = action;
+	}
+	static void reset_alarm() { alarm = 0; alarm_action = 0; }
+	static void reset_to_ap() {
+		options[OPTION_MOD].ival = OG_MOD_AP;
+		options_save();
+		restart();
+	}
+	static void config_ip();
+	static void play_startup_tune();
+	private:
+	static File log_file;
+	static void button_handler();
+	static void led_handler();
+
+	static DallasTemperature *ds18b20;
+	static DHTesp* dht;
+};
+
+#endif
